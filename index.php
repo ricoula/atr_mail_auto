59e093db76805cbb0beecea5f0ba8469f39dfad9
Florian Spadaro <spadaro.florian@outlook.fr>
2017-09-25 15:41:38 +0200

    début

--- index.php
+++ index.php
@@ -4,9 +4,29 @@
         <meta charset="utf-8" />
         <title>Mail</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        
+        <style>
+            #divSousJustifs{
+                display: none;
+            }
+        </style>
     </head>
     <body>
-        
+        <div class="container">
+            <form>
+                <div class="form-group jumbotron" id="divUi">
+                </div>
+                <div class="form-group jumbotron" id="divDomaines">
+                </div>
+                <div class="form-group jumbotron" id="divSousDomaines">
+                </div>
+                <div class="form-group jumbotron" id="divPg">
+                </div>
+                <div class="form-group jumbotron" id="divSousJustifs">
+                </div>
+            </form>
+            
+        </div>
         
         
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
