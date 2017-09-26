d39885116c9680c681e2e1564c87743c9edb79ac
Cyril RICOU <cyril.ricou@Ambition.local>
2017-09-26 10:52:50 +0200

    changeme,t

--- API/connexionBdd.php
+++ API/connexionBdd.php
@@ -1,10 +1,6 @@
 <?php
 	try{
-<<<<<<< HEAD
 		$bdd = new PDO('pgsql:host=192.168.30.240;dbname=ambigroup_dev', 'admambigroup', '13jkgaUM8Um');
-=======
-		$bdd = new PDO('pgsql:host=192.168.30.240;dbname=ambigroup_dev','admambigroup','13jkgaUM8Um');
->>>>>>> 96ee55dfac2a0e4bbf0af3a9f97128a2e29358b0
 	}
 	catch (Exception $e){
 		die('Erreur : '.$e->getMessage());
