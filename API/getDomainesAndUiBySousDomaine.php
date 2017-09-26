f61c46edeebd94a57ca219fe54d50771d1ee9f33
Florian Spadaro <spadaro.florian@outlook.fr>
2017-09-26 13:04:04 +0200

    PAS TERMINE

---
+++ API/getDomainesAndUiBySousDomaine.php
@@ -0,0 +1,4 @@
+<?php
+	require_once("fonctions.php");
+	echo getDomainesAndUiBySousDomaine($_POST["sous_domaine"]);
+?>
---
+++ API/getSousDomainesAndDomainesAndUiBySousJustif.php
@@ -0,0 +1,5 @@
+<?php
+	require_once("fonctions.php");
+	//echo getSousDomainesAndDomainesAndUiBySousJustif($_POST["sous_justif"]);
+	echo getSousDomainesAndDomainesAndUiBySousJustif("N5");
+?>
