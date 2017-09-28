fd1f50617659de057d0ae3a74dd1ed897913d9b4
Cyril RICOU <cyril.ricou@Ambition.local>
2017-09-28 09:39:26 +0200

    test

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
                     }
                     ?>
                 </div>
-                <button id="btnValiderFiltres" class="btn btn-info">Valider filtres</button>
+                <!--<button id="btnValiderFiltres" class="btn btn-info">Valider filtres</button>-->
             </form>
        
             <br/>
