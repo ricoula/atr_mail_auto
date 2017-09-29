1bd97b3fd9472a8a592e06a15794310450d3255e
Cyril RICOU <cyril.ricou@Ambition.local>
2017-09-29 11:55:40 +0200

    ee

--- index.php
+++ index.php
@@ -138,6 +138,6 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="tablesort/jquery.tablesorter.min.js"></script>
         <script src="js/index.js"></script>
-        <!--<script src="js/checkbox.js"></script>-->
+        <script src="js/checkbox.js"></script>
     </body>
 </html>
